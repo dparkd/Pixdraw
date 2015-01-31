2a42f2a3e00f5dad15280fb772c826b1afab987a
Daniel Park <dpark@Daniels-MacBook-Pro.local>
2015-01-31 12:41:10 -0800

    add drag feature

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -2,6 +2,8 @@
 var boxClass = [];
 var currentColor = '#22A7F0';
 var cursorColor = 'blue.png';
+var isDragging = false;
+
 
 //Dom Ready =============================
 $(document).ready(function(){
@@ -11,17 +13,24 @@ $(document).ready(function(){
 
 	$('#colorPicker a').on('click', chooseColor);
 
+	$('.box').on('mousedown', startDrag);
+	$('.box').on('mouseup', removeDrag);
+
+	$('.box').on('mouseenter', enterBox);
+
 
 });
 
 
 //Functions =============================
+//Change color on click
 function boxClicked() {
 
 	$(this).attr('data-background', currentColor);
 
 }
 
+//Choose a color
 function chooseColor() {
 
 	currentColor = $(this).attr('data-color');
@@ -29,3 +38,34 @@ function chooseColor() {
 	$('body').css('cursor', 'url(../images/' + cursorColor + '), auto');
 
 }
+
+//draggingMouse
+function startDrag() {
+	isDragging = true;
+	console.log('isDraggin');
+}
+function removeDrag() {
+	isDragging = false;
+	console.log('nodrag');
+}
+
+//Enter box
+function enterBox() {
+	
+	if (isDragging) {
+		$(this).attr('data-background', currentColor);
+	}
+	else{
+		return;
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
