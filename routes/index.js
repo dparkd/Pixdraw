e65e0bd87cb7222b6e712846ee2859fca5f0151b
Daniel Park <dpark@Daniels-MacBook-Pro.local>
2015-02-08 23:35:58 -0800

    add pixel and canvas size settings

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,52 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'PixDraw' });
 });
 
-router.get('/draw', function(req, res, next) {
-  res.render('draw', { title: 'PixDraw - Draw Square' });
+// router.get('/draw', function(req, res, next) {
+//   res.render('draw', { title: 'PixDraw - Draw Square' });
+// });
+
+router.get('/settings', function(req, res, next) {
+  res.render('settings', { title: 'PixDraw - Setup' });
 });
 
-router.get('/draw2', function(req, res, next) {
-  res.render('draw2', { title: 'Drawing App' });
+router.post('/draw', function(req, res) {
+	var canvasSize = req.body.canvasSize;
+	var pixelSize = req.body.pixelSize;
+
+	//Determine the aspect ratio of the canvas
+	if (canvasSize == 'one') {
+		var height = '400';
+		var width = '400';
+		//Determine the pixel size
+		if (pixelSize == '20px'){
+			var pixelNumber = 400;
+		}
+		else if (pixelSize == '10px'){
+			var pixelNumber = 1600;
+		}
+		else if (pixelSize == '5px'){
+			var pixelNumber = 6400;
+		}
+	} else if (canvasSize == 'two') {
+		var height = '300';
+		var width = '400';
+		//Determine the pixel size
+		if (pixelSize == '20px'){
+			var pixelNumber = 300;
+		}
+		else if (pixelSize == '10px'){
+			var pixelNumber = 1200;
+		}
+		else if (pixelSize == '5px'){
+			var pixelNumber = 4800;
+		}
+	}
+
+	
+
+	res.render('draw', {title:'PixDraw - Draw', height:height, width:width, pixelNumber:pixelNumber, canvasSize:canvasSize, pixelSize:pixelSize});
+
+	
 });
 
 router.get('/features', function(req, res, next) {
